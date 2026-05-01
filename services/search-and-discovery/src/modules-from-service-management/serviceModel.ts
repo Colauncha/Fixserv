ec2cf5b976910ebc98aeecaab6180a25c47554b0
OGKpos <evwerhamreisrael@gmail.com>
2026-05-01 17:27:27 +0100

    fixed events-sync DB collection name

--- services/search-and-discovery/src/modules-from-service-management/serviceModel.ts
+++ services/search-and-discovery/src/modules-from-service-management/serviceModel.ts
@@ -7,13 +7,14 @@ const ServiceSchema = new mongoose.Schema(
     artisanId: { type: String, ref: "ArtisanModel", required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
+    bio: { type: String },
     price: { type: Number, min: 0, required: true },
     estimatedDuration: { type: String, required: true },
     isActive: { type: Boolean, default: true },
     rating: { type: Number, default: 0, min: 0, max: 5, required: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-const ServiceModel = mongoose.model("ServiceModel", ServiceSchema);
+const ServiceModel = mongoose.model("Search_Services", ServiceSchema);
 export { ServiceModel };
